f32eed056834b7cc0a576ddfb0acf9340578735e
Kyamel <lcamelo200@gmail.com>
2026-07-20 17:41:07 -0300

    l3 q4

--- l3/Q4.hs
+++ l3/Q4.hs
@@ -1,12 +1,28 @@
 module Q4 where
 
-  data LE = And LE LE
-    | Or LE LE
-    | Not LE
-    | Const Bool
-    | Var String
-    deriving Show
+    data LE = And LE LE
+        | Or LE LE
+        | Not LE
+        | Const Bool
+        | Var String
+        deriving Show
 
-  eval :: [(String,Bool)] -> LE -> Maybe Bool
-  eval [(x:xs, b)] Or = eval [(xs,b)] le || eval((xs,b) le)
+    eval :: [(String, Bool)] -> LE -> Maybe Bool
+    eval ambiente (Const b) = Just b
 
+    eval ambiente (Var nome) =
+    lookup nome ambiente
+
+    eval ambiente (Not expressao) = do
+    valor <- eval ambiente expressao
+    return (not valor)
+
+    eval ambiente (And esquerda direita) = do
+    valorEsquerda <- eval ambiente esquerda
+    valorDireita  <- eval ambiente direita
+    return (valorEsquerda && valorDireita)
+
+    eval ambiente (Or esquerda direita) = do
+    valorEsquerda <- eval ambiente esquerda
+    valorDireita  <- eval ambiente direita
+    return (valorEsquerda || valorDireita)
