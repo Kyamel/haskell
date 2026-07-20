11da5a2c3da043abb776b5736e10e08d0870267d
Kyamel <lcamelo200@gmail.com>
2026-07-20 17:45:25 -0300

    l3 q4 zipMaybe

--- l3/Q4.hs
+++ l3/Q4.hs
@@ -8,21 +8,53 @@ module Q4 where
         deriving Show
 
     eval :: [(String, Bool)] -> LE -> Maybe Bool
-    eval ambiente (Const b) = Just b
+    eval _ (Const b) = Just b
 
     eval ambiente (Var nome) =
     lookup nome ambiente
 
-    eval ambiente (Not expressao) = do
-    valor <- eval ambiente expressao
-    return (not valor)
+    eval ambiente (And e1 e2) =
+    zipMaybe (&&) (eval ambiente e1) (eval ambiente e2)
 
-    eval ambiente (And esquerda direita) = do
-    valorEsquerda <- eval ambiente esquerda
-    valorDireita  <- eval ambiente direita
-    return (valorEsquerda && valorDireita)
+    eval ambiente (Or e1 e2) =
+    zipMaybe (||) (eval ambiente e1) (eval ambiente e2)
 
-    eval ambiente (Or esquerda direita) = do
-    valorEsquerda <- eval ambiente esquerda
-    valorDireita  <- eval ambiente direita
-    return (valorEsquerda || valorDireita)
+    eval ambiente (Not e) =
+    mapMaybe not (eval ambiente e)
+
+    zipMaybe :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+    zipMaybe f (Just x) (Just y) = Just (f x y)
+    zipMaybe _ _ _ = Nothing
+
+    mapMaybe :: (a -> b) -> Maybe a -> Maybe b
+    mapMaybe f (Just x) = Just (f x)
+    mapMaybe _ Nothing  = Nothing
+
+
+    vars :: LE -> [String]
+    vars (Const _)   = []
+    vars (Var nome)  = [nome]
+    vars (Not e)     = vars e
+    vars (And e1 e2) = vars e1 ++ vars e2
+    vars (Or e1 e2)  = vars e1 ++ vars e2
+
+
+    instance Eq LE where
+        Const b1 == Const b2 =
+            b1 == b2
+
+        Var nome1 == Var nome2 =
+            nome1 == nome2
+
+        Not e1 == Not e2 =
+            e1 == e2
+
+        And e1 e2 == And e3 e4 =
+            e1 == e3 && e2 == e4
+
+        Or e1 e2 == Or e3 e4 =
+            e1 == e3 && e2 == e4
+
+        _ == _ =
+            False
+
